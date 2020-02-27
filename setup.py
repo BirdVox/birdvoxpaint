4d1028934807273158ad8c976db1377ef0ce442e
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2020-02-26 22:43:02 -0500

    add tqdm to requirements

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(name='birdvoxpaint',
                  install_requires=[
                     'librosa>=0.7.0',
                     'matplotlib',
+                    'tqdm'
                  ],
                  entry_points={
                         # 'console_scripts': [
