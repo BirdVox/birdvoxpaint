197168ccf244299f9246277408e2c1e89c636c68
Vincent <vincent.lostanlen@nyu.edu>
2020-03-18 10:38:58 -0400

    update indices

--- birdvoxpaint/indices.py
+++ birdvoxpaint/indices.py
@@ -72,9 +72,9 @@ def towsey_rgb(S, **kwargs):
     entropy_based_concentration = 1 - rowwise_entropy / np.log(S.shape[1])
 
     # Compute count of acoustic event count as proportion of frames above background noise.
-    acoustic_event_threshold = 1.0 * background_noise_level / S.shape[1]
+    acoustic_event_threshold = 2.0*np.mean(S, axis=1)
     acoustic_event_count = np.mean(
-        (S > acoustic_event_threshold[:, np.newaxis]), axis=1)
+        (S > acoustic_event_threshold[:, np.newaxis]).astype('float'), axis=1)
 
     # Stack ACI, ENT, and CVR into a tensor.
     rgb_tensor = np.stack([
