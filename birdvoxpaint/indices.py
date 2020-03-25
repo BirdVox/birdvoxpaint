50e3e0d6743e9892c2e17c9e04f369fb8d833ed6
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2020-03-25 13:01:12 +0100

    delete towsey_rgb

--- birdvoxpaint/indices.py
+++ birdvoxpaint/indices.py
@@ -48,38 +48,4 @@ def maximum_flux(S):
 def maximum_pcen(S, **kwargs):
     S = np.abs(S) ** 2
     pcen = librosa.pcen(S, **kwargs)
-    return np.max(pcen, axis=1)[np.newaxis, :]
-
-
-def towsey_rgb(S, **kwargs):
-    # Compute spectrogram as squared complex modulus.
-    S = np.abs(S) ** 2
-
-    # Compute spectral flux.
-    flux = np.abs(np.diff(S, axis=1))
-
-    # Note that, contrary to Towsey et al., we estimate background noise by averaging
-    # amplitudes over time. This "pink noise model" may not reflect the perceived level of
-    # noise in eco-acoustic recordings, but it is conceptually simpler (and faster) than
-    # the approach of Towsey, which involves the computation of quantiles.
-    background_noise_level = np.sum(S, axis=1)
-
-    # Compute acoustic complexity index, as quotient of averaged flux to averaged energy.
-    acoustic_complexity_index = np.sum(flux, axis=1) / background_noise_level
-
-    # Compute temporal entropy index.
-    rowwise_entropy = np.array([stats.entropy(row) for row in S])
-    entropy_based_concentration = 1 - rowwise_entropy / np.log(S.shape[1])
-
-    # Compute count of acoustic event count as proportion of frames above background noise.
-    acoustic_event_threshold = 2.0*np.mean(S, axis=1)
-    acoustic_event_count = np.mean(
-        (S > acoustic_event_threshold[:, np.newaxis]).astype('float'), axis=1)
-
-    # Stack ACI, ENT, and CVR into a tensor.
-    rgb_tensor = np.stack([
-        acoustic_complexity_index,
-        entropy_based_concentration,
-        acoustic_event_count
-    ])
-    return rgb_tensor
+    return np.max(pcen, axis=1)
