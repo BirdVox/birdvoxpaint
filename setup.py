53fe649eff85e2a5e18e23a799fe5cdd72ebcffa
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2020-03-28 00:03:21 +0100

    require librosa>=0.7.1, set version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='birdvoxpaint',
-    version='0.2.0',
+    version='0.2.1',
     description='Bird Vox False-Color Spectrograms',
     long_description=open('README.md').read().strip(),
     long_description_content_type="text/markdown",
@@ -13,7 +13,7 @@ setuptools.setup(
     # py_modules=['packagename'],
     # package_data={'asdf': {'*.yaml'}},
     # include_package_data=True,
-    install_requires=['librosa>=0.7.0', 'matplotlib', 'tqdm'],
+    install_requires=['librosa>=0.7.1', 'matplotlib', 'tqdm'],
     entry_points={
         # 'console_scripts': [
         #     'bvpaint = bvpaint.cli:main'
